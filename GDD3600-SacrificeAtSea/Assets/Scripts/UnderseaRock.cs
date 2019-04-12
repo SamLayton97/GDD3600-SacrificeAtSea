6c250801231ed49e5fd3e023615cc7c7328f6b82
Sam Layton <slayton@uccs.edu>
2019-04-11 22:34:53 -0600

    Removed cavern wall spawning. Undersea rocks now randomize their sprite when they spawn.

---
+++ GDD3600-SacrificeAtSea/Assets/Scripts/UnderseaRock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script controlling force, sprite, and rotation of undersea rock.
+/// </summary>
+public class UnderseaRock : MonoBehaviour
+{
+    // sprite swapping support
+    SpriteRenderer mySpriteRenderer;
+    [SerializeField] Sprite[] underseaRockSprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // randomize undersea rock's sprite
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        int randSpriteIndex = Random.Range((int)0, (int)underseaRockSprites.Length);
+        mySpriteRenderer.sprite = underseaRockSprites[randSpriteIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GDD3600-SacrificeAtSea/Assets/Scripts/UnderseaRockSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Spawns rocks at random points just outside navigation panel
+/// </summary>
+public class UnderseaRockSpawner : MonoBehaviour
+{
+    // spawn support
+    [SerializeField] GameObject underseaRockPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
