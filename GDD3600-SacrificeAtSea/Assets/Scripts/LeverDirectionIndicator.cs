f18f29135ea2de98d4574dc87bac102ded85c6a3
Sam Layton <slayton@uccs.edu>
2019-04-12 09:47:42 -0600

    Added signs to environmental UI that indicate what direction levers move submarine in. These currently do not respond to updates in part functionality.

---
+++ GDD3600-SacrificeAtSea/Assets/Scripts/LeverDirectionIndicator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Environmental sign that indicates both direction lever moves
+/// submarine in and whether its corresponding nav capabilities
+/// are working.
+/// </summary>
+public class LeverDirectionIndicator : MonoBehaviour
+{
+    // functionality support
+    [SerializeField] SubmarineParts myCorrespondingPart = SubmarineParts.ballastTanks;
+
+    // sprite swapping support
+    SpriteRenderer mySpriteRenderer;
+    [SerializeField] Sprite enabledSprite;
+    [SerializeField] Sprite disabledSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // retrieve sprite renderer component from game object
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+
+        // add self as listener to Update Part Functionality event
+        EventManager.AddUpdateFunctionalityListeners(UpdateFunctionality);
+    }
+
+    // Updates sprite to reflect whether its corresponding part is enabled
+    void UpdateFunctionality(SubmarineParts updatedPart, bool isEnabled)
+    {
+
+    }
+}
