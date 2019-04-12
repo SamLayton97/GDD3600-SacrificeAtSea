58c3032525452094358939ecaa9e84f26ad117b5
Sam Layton <slayton@uccs.edu>
2019-04-11 23:07:32 -0600

    Newly spawned undersea rocks generate angle in general direction of their target

--- GDD3600-SacrificeAtSea/Assets/Scripts/UnderseaRock.cs
+++ GDD3600-SacrificeAtSea/Assets/Scripts/UnderseaRock.cs
@@ -11,13 +11,43 @@ public class UnderseaRock : MonoBehaviour
     SpriteRenderer mySpriteRenderer;
     [SerializeField] Sprite[] underseaRockSprites;
 
+    // force support
+    Rigidbody2D myRigidbody2D;
+    [SerializeField] float impulseForceScale = 1f;
+    [SerializeField] float forceAngleArc = 35f;
+    Transform target;
+
+    /// <summary>
+    /// Property with write access containing target's transform info.
+    /// </summary>
+    public Transform Target
+    {
+        set { target = value; }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        // randomize undersea rock's sprite
+        // retrieve references to various components
         mySpriteRenderer = GetComponent<SpriteRenderer>();
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+
+        // randomize undersea rock's sprite
         int randSpriteIndex = Random.Range((int)0, (int)underseaRockSprites.Length);
         mySpriteRenderer.sprite = underseaRockSprites[randSpriteIndex];
+
+        // find angle to target
+        float deltaX = target.position.x - transform.position.x;
+        float deltaY = target.position.y - transform.position.y;
+        float angleToTarget = Mathf.Atan2(deltaY, deltaX) * Mathf.Rad2Deg;
+
+        // apply randomized offset to angle within specified arc
+        float halfArc = forceAngleArc / 2;
+        float randOffset = Random.Range(-halfArc, halfArc);
+        angleToTarget += randOffset;
+
+        // TODO: apply impulse force in general direction of player's submarine
+        
     }
 
     // Update is called once per frame
--- GDD3600-SacrificeAtSea/Assets/Scripts/UnderseaRockSpawner.cs
+++ GDD3600-SacrificeAtSea/Assets/Scripts/UnderseaRockSpawner.cs
@@ -10,10 +10,16 @@ public class UnderseaRockSpawner : MonoBehaviour
     // spawn support
     [SerializeField] GameObject underseaRockPrefab;
 
+    // targeting support
+    [SerializeField] Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // DEBUGGING: spawn undersea rock to test force functionality
+        GameObject newRock = Instantiate(underseaRockPrefab,
+            new Vector2(16, 0.5f), Quaternion.identity);
+        newRock.GetComponent<UnderseaRock>().Target = target;
     }
 
     // Update is called once per frame
