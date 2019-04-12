8d495960ec0c6c1d773dfda9aeabc3bef072d90b
Sam Layton <slayton@uccs.edu>
2019-04-11 23:46:28 -0600

    Added timer to facilitate randomly delayed rock spawns

--- GDD3600-SacrificeAtSea/Assets/Scripts/UnderseaRockSpawner.cs
+++ GDD3600-SacrificeAtSea/Assets/Scripts/UnderseaRockSpawner.cs
@@ -9,6 +9,10 @@ public class UnderseaRockSpawner : MonoBehaviour
 {
     // spawn support
     [SerializeField] GameObject underseaRockPrefab;
+    [SerializeField] float minSpawnDelay = 5f;
+    [SerializeField] float maxSpawnDelay = 10f;
+    float nextSpawnCounter = 0;
+    float randSpawnDelay = 0;
 
     // targeting support
     [SerializeField] Transform target;
@@ -16,12 +20,24 @@ public class UnderseaRockSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // generate random delay for first rock spawn
+        randSpawnDelay = Random.Range(minSpawnDelay, maxSpawnDelay);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // if spawn counter exceeds delay time
+        if (nextSpawnCounter > randSpawnDelay)
+        {
+            // reset spawn counter and generate new delay time
+            nextSpawnCounter = 0;
+            randSpawnDelay = Random.Range(minSpawnDelay, maxSpawnDelay);
+
+            // TODO: spawn rock at one of nav panel sides
+        }
+
+        // increment counter by time passed between frames
+        nextSpawnCounter += Time.deltaTime;
     }
 }
