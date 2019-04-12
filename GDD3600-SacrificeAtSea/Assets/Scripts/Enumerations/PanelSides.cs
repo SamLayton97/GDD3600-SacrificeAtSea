af0fa407d30fa2a4bee1fd7ab95de0f4159d27c1
Sam Layton <slayton@uccs.edu>
2019-04-12 00:14:49 -0600

    Rocks now spawn from sides of screen

---
+++ GDD3600-SacrificeAtSea/Assets/Scripts/Enumerations/PanelSides.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Enumeration of navigation panel sides to spawn obstacles from
+/// </summary>
+public enum PanelSides
+{
+    Left,
+    Right,
+    Top,
+    Bottom
+}
--- GDD3600-SacrificeAtSea/Assets/Scripts/UnderseaRockSpawner.cs
+++ GDD3600-SacrificeAtSea/Assets/Scripts/UnderseaRockSpawner.cs
@@ -9,6 +9,11 @@ public class UnderseaRockSpawner : MonoBehaviour
 {
     // spawn support
     [SerializeField] GameObject underseaRockPrefab;
+    Vector2 panelSize;
+    float halfPanelWidth = 0;
+    float halfPanelHeight = 0;
+
+    // spawn delay support
     [SerializeField] float minSpawnDelay = 5f;
     [SerializeField] float maxSpawnDelay = 10f;
     float nextSpawnCounter = 0;
@@ -20,6 +25,11 @@ public class UnderseaRockSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // retrieve sprite renderer from game object
+        panelSize = GetComponent<SpriteRenderer>().size;
+        halfPanelWidth = (panelSize.x / 2) - .1f;
+        halfPanelHeight = (panelSize.y / 2) - .1f;
+
         // generate random delay for first rock spawn
         randSpawnDelay = Random.Range(minSpawnDelay, maxSpawnDelay);
     }
@@ -30,11 +40,41 @@ public class UnderseaRockSpawner : MonoBehaviour
         // if spawn counter exceeds delay time
         if (nextSpawnCounter > randSpawnDelay)
         {
+            Debug.Log("spawn");
+
             // reset spawn counter and generate new delay time
             nextSpawnCounter = 0;
             randSpawnDelay = Random.Range(minSpawnDelay, maxSpawnDelay);
 
-            // TODO: spawn rock at one of nav panel sides
+            // generate random spawn position
+            float randSpawnX = Random.Range(-halfPanelWidth, halfPanelWidth);
+            float randSpawnY = Random.Range(-halfPanelHeight, halfPanelHeight);
+
+            // clamp spawn position to one of panel sides
+            PanelSides randSide = (PanelSides)Random.Range((int)0, (int)4);
+            switch (randSide)
+            {
+                case PanelSides.Left:
+                    randSpawnX = -halfPanelWidth;
+                    break;
+                case PanelSides.Right:
+                    randSpawnX = halfPanelWidth;
+                    break;
+                case PanelSides.Bottom:
+                    randSpawnY = -halfPanelHeight;
+                    break;
+                case PanelSides.Top:
+                    randSpawnY = halfPanelHeight;
+                    break;
+                default:
+                    break;
+            }
+            Vector3 spawnPosition = new Vector3(randSpawnX, randSpawnY);
+
+            // spawn rock at clamped position and initialize its target
+            GameObject newRock = Instantiate(underseaRockPrefab, 
+                transform.position + spawnPosition, Quaternion.identity);
+            newRock.GetComponent<UnderseaRock>().Target = target;
         }
 
         // increment counter by time passed between frames
