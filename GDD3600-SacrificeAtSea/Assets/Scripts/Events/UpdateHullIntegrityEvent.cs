91a9a1e532fd5aa16e491c5f6c361501176dc39c
Sam Layton <slayton@uccs.edu>
2019-04-12 23:00:36 -0600

    Added event to update hull integrity

---
+++ GDD3600-SacrificeAtSea/Assets/Scripts/Events/UpdateHullIntegrityEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Event to update hull integrity/submarine health
+/// </summary>
+public class UpdateHullIntegrityEvent : UnityEvent<int>
+{
+}
--- GDD3600-SacrificeAtSea/Assets/Scripts/ProgressUI.cs
+++ GDD3600-SacrificeAtSea/Assets/Scripts/ProgressUI.cs
@@ -1,13 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
+/// <summary>
+/// Script used to update user-interface tracking submarine's
+/// health and progress towards goal
+/// </summary>
 public class ProgressUI : MonoBehaviour
 {
+    // hull integrity support
+    [SerializeField] Text hullIntegrityText;
+    const string HullIntegrityTextPrefix = "HULL: ";
+    int hullIntegrity = 100;
+
+    // level progress support
+    [SerializeField] Text goalProgressText;
+    const string GoalProgressTextPrefix = "PROGRESS: ";
+    int goalProgress = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // initialize UI text
+        hullIntegrityText.text = HullIntegrityTextPrefix + hullIntegrity;
+        goalProgressText.text = GoalProgressTextPrefix + goalProgress;
     }
 
     // Update is called once per frame
