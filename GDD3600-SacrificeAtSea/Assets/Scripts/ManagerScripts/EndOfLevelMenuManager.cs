6538495eff1d68854696744e3f6eda8d853fce55
Sam Layton <slayton@uccs.edu>
2019-04-13 01:00:12 -0600

    Added basic end-of-level scenes to go to when player reaches either win or fail state

---
+++ GDD3600-SacrificeAtSea/Assets/Scripts/ManagerScripts/EndOfLevelMenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Manager that facilitates scene movement within end of level menu
+/// </summary>
+public class EndOfLevelMenuManager : MonoBehaviour
+{
+    // Handles when player clicks 'Replay' button
+    public void HandleReplayButtonClickEvent()
+    {
+        // go to submarine/gameplay scene
+        SceneManager.LoadScene("SubmarineScene");
+    }
+
+    // Handles when player clicks 'Main Menu' button
+    public void HandleMainMenuButtonClickEvent()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
