abbc2ec43ce3e3b82f1e5b4c28df1677dde37ce8
Sam Layton <slayton@uccs.edu>
2019-04-12 21:03:03 -0600

    Added static health bar above repair terminals

---
+++ GDD3600-SacrificeAtSea/Assets/Scripts/Interactables Scripts/LocalHealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Controls health bar on canvas local to game object
+/// </summary>
+public class LocalHealthBar : MonoBehaviour
+{
+    // health bar support fields
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
