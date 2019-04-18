a967a0c4b87d5114f0d0ab1695bb2ed2106cbdcc
Sam Layton <slayton@uccs.edu>
2019-04-18 12:04:34 -0600

    Added script to control the visibility of objects on the navigation panel

---
+++ GDD3600-SacrificeAtSea/Assets/Scripts/Navigation Panel Scripts/SonarPingResponse.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Causes nav panel object to pulsate and appear difficult to see
+/// when submarine's sonar is malfunctioning.
+/// </summary>
+public class SonarPingResponse : MonoBehaviour
+{
+    // define visibility variables
+    [SerializeField] float maxFunctioningOpacity;
+    [SerializeField] float minFunctioningOpacity;
+    [SerializeField] float maxMalfunctioningOpacity;
+    [SerializeField] float minMalfuctioningOpacity;
+
+    // ping speed control
+
+
+    // visibility support variables
+    SpriteRenderer mySpriteRenderer;
+    bool isSonarFunctioning = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
