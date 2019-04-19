6b48568e27ba874ce09c9a4a7f94bd97fd554319
Sam Layton <slayton@uccs.edu>
2019-04-19 16:59:22 -0600

    Separated end of level panel into complete and failed level prefabs

---
+++ GDD3600-SacrificeAtSea/Assets/Scripts/Events/LevelEndEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Event triggered when player has reached one of level's end conditions.
+/// </summary>
+public class LevelEndEvent : UnityEvent<int, int, int, float>
+{
+}
