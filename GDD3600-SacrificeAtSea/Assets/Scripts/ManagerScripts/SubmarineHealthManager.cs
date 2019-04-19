7c25c3f9e5c8aad8f46e44babce6de2267ea5afd
Sam Layton <slayton@uccs.edu>
2019-04-18 20:21:28 -0600

    Added screen-shake when submarine collides with a cavern obstacle

--- GDD3600-SacrificeAtSea/Assets/Scripts/ManagerScripts/SubmarineHealthManager.cs
+++ GDD3600-SacrificeAtSea/Assets/Scripts/ManagerScripts/SubmarineHealthManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using EZCameraShake;
 
 /// <summary>
 /// Script managing submarine's overall health/hull integrity
@@ -18,6 +19,12 @@ public class SubmarineHealthManager : MonoBehaviour
 
     // event support
     UpdateHullIntegrityEvent updateHullIntegrityEvent;
+
+    // screen shake support variables
+    [SerializeField] float shakeMagnitude = 4f;
+    [SerializeField] float shakeRoughness = 4f;
+    [SerializeField] float shakeFadeInTime = .1f;
+    [SerializeField] float shakeFadeOutTime = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -36,6 +43,9 @@ public class SubmarineHealthManager : MonoBehaviour
         // decrement health and send out updated hull integrity (locked to 0 or greater)
         currHealth = Mathf.Max(currHealth - damagePerCollision, 0);
         updateHullIntegrityEvent.Invoke(currHealth);
+
+        // shake screen
+        CameraShaker.Instance.ShakeOnce(shakeMagnitude, shakeRoughness, shakeFadeInTime, shakeFadeOutTime);
 
         // if health drops below 0, Game Over
         if (currHealth <= 0)
