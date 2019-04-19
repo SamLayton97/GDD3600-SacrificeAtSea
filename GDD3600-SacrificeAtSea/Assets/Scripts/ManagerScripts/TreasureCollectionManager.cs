8e38368a857a22a97331fdd316117fa79ec0931d
Sam Layton <slayton@uccs.edu>
2019-04-18 23:34:23 -0600

    Added treasure collection manager that tracks how much treasure/gold player has accumulated over course of level.

---
+++ GDD3600-SacrificeAtSea/Assets/Scripts/ManagerScripts/TreasureCollectionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script managing overhead treasure collection.
+/// </summary>
+public class TreasureCollectionManager : MonoBehaviour
+{
+    [SerializeField] int goldValue = 100;
+
+    int currGold = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // add self as listener to "Collect Treasure" event
+        EventManager.AddCollectTreasureListener(HandleTreasureCollection);
+    }
+
+    /// <summary>
+    /// Listens for "Collect Treasure" event and adds gold as appropriate
+    /// </summary>
+    void HandleTreasureCollection()
+    {
+        // add gold value
+        currGold += goldValue;
+        Debug.Log(currGold);
+    }
+}
