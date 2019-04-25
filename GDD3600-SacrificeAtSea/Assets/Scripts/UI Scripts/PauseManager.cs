2816ba59c73fd24ae43c1e5e08c1cd096a3bfa12
Sam Layton <slayton@uccs.edu>
2019-04-25 12:37:06 -0600

    Added pause manager script to facilitate pausing of game (during level). Made prefab out of game manager for easy modification of it.

---
+++ GDD3600-SacrificeAtSea/Assets/Scripts/UI Scripts/PauseManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages pausing of game
+/// </summary>
+public class PauseManager : MonoBehaviour
+{
+    // pause menu support variables
+    [SerializeField] GameObject pauseMenu;
+
+    /// <summary>
+    /// Property returning whether game is currently paused
+    /// </summary>
+    public bool IsGamePaused
+    {
+        get
+        {
+            // if time scale is 0, return true
+            if (Time.timeScale == 0)
+                return true;
+            // otherwise, return false
+            else
+                return false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if user enters pause input and game is not already paused
+        if (Input.GetAxisRaw("Pause") != 0 && !IsGamePaused)
+        {
+            // TODO: create instance of pause menu
+
+        }
+    }
+}
