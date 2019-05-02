6dfc43c64e78c98eb897f0a13226d017b5648b7d
Sam Layton <slayton@uccs.edu>
2019-05-02 12:05:30 -0600

    Added scene to prime users on theme of game.

---
+++ GDD3600-SacrificeAtSea/Assets/Scripts/Menu Navigation Scripts/PrimerMenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Facilitates scene movement from "Thematic Primer" menu.
+/// </summary>
+public class PrimerMenuManager : MonoBehaviour
+{
+    // scene transition support variables
+    [SerializeField] string fromPlaySceneName = "";
+    [SerializeField] string fromBackSceneName = "";
+
+    // Handles when player clicks "play" button
+    public void HandlePlayButtonOnClick()
+    {
+        // go to first gameplay scene
+        SceneManager.LoadScene(fromPlaySceneName);
+    }
+
+    // Handles when player clicks 'back' button
+    public void HandleBackButtonOnClick()
+    {
+        // return to title screen
+        SceneManager.LoadScene(fromBackSceneName);
+    }
+}
