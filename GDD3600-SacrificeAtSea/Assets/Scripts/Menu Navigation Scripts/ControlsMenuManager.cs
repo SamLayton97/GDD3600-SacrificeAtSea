b5307d0376bde5ddcf79a2e18409734e62fa48f7
Sam Layton <slayton@uccs.edu>
2019-05-02 11:26:15 -0600

    Added functional controls page

---
+++ GDD3600-SacrificeAtSea/Assets/Scripts/Menu Navigation Scripts/ControlsMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Facilitates scene movement from Controls page
+/// </summary>
+public class ControlsMenuManager : MonoBehaviour
+{
+    // scene movement support varables
+    [SerializeField] string titleScreenSceneName = "";
+
+    // Handles when user clicks "Back" button
+    public void HandleBackButtonClickEvent()
+    {
+        // return to title screen
+        SceneManager.LoadScene(titleScreenSceneName);
+    }
+}
