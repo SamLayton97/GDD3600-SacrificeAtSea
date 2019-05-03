5414857c77e32f38e0bf9d490916c7efa51f1088
Sam Layton <slayton@uccs.edu>
2019-05-02 22:08:15 -0600

    Tutorial manager enters next tutorial stage when triggers are hit

--- GDD3600-SacrificeAtSea/Assets/Scripts/Manager Scripts/TutorialManager.cs
+++ GDD3600-SacrificeAtSea/Assets/Scripts/Manager Scripts/TutorialManager.cs
@@ -10,23 +10,54 @@ public class TutorialManager : MonoBehaviour
     // phase support variables
     TutorialStages currentStage = TutorialStages.InitialSteps;
 
-    /// <summary>
-    /// Read-access property returning current stage of tutorial
-    /// </summary>
-    TutorialStages CurrentStage
-    {
-        get { return currentStage; }
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        // add self as listener to trigger next stage event
+        EventManager.AddTriggerNextStageListener(TriggerNextStage);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// Triggers next stage of tutorial
+    /// </summary>
+    /// <param name="nextStage">stage to enter</param>
+    void TriggerNextStage(TutorialStages nextStage)
+    {
+        // if triggered stage is past current, enter it
+        if (nextStage > currentStage)
+        {
+            // update current stage to one triggered
+            currentStage = nextStage;
+
+            // TODO: enter triggered stage
+            switch (nextStage)
+            {
+                case TutorialStages.InitialSteps:
+                    Debug.Log("ERROR: Uninitialized trigger.");
+                    break;
+                case TutorialStages.Introduction:
+                    break;
+                case TutorialStages.FirstRepair:
+                    break;
+                case TutorialStages.RepairRest:
+                    break;
+                case TutorialStages.FirstMineVolley:
+                    break;
+                case TutorialStages.SecondMineVolley:
+                    break;
+                case TutorialStages.ThirdMineVolley:
+                    break;
+                case TutorialStages.RepairAgain:
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
