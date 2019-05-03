53fb00c63224e9c55759f10f256b3fa0f92e669d
Sam Layton <slayton@uccs.edu>
2019-05-03 04:29:05 -0600

    Added credits menu

---
+++ GDD3600-SacrificeAtSea/Assets/Scripts/Menu Navigation Scripts/CreditsMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Facilitates scene movement from credits menu
+/// </summary>
+public class CreditsMenuManager : MonoBehaviour
+{
+    // scene transition support variables
+    [SerializeField] string fromBackSceneName = "";
+
+    // Handles when player clicks 'back' button
+    public void HandleBackButtonOnClick()
+    {
+        // return to title screen
+        SceneManager.LoadScene(fromBackSceneName);
+    }
+}
