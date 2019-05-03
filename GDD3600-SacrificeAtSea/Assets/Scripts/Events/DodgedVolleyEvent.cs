b86b281343447f6367a9dfe644be59089c84b91a
Sam Layton <slayton@uccs.edu>
2019-05-03 01:55:15 -0600

    Added event to transition to next tutorial stage after dodging a volley of mines

---
+++ GDD3600-SacrificeAtSea/Assets/Scripts/Events/DodgedVolleyEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Event triggered when player succeeds in dodging
+/// pre-determined volley of sea mines.
+/// </summary>
+public class DodgedVolleyEvent : UnityEvent
+{
+}
