1b4df7e0c872b7f9e53941f529f2b1a7e5cb4cdf
Sam Layton <slayton@uccs.edu>
2019-05-02 19:54:49 -0600

    Added enumeration of tutorial stages -- facilitates sequencing of tutorial level

---
+++ GDD3600-SacrificeAtSea/Assets/Scripts/Enumerations/TutorialStages.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Enumeration of stages of tutorial
+/// </summary>
+public enum TutorialStages
+{
+    InitialSteps,
+    Introduction,
+    FirstRepair,
+    RepairRest,
+    FirstMineVolley,
+    SecondMineVolley,
+    ThirdMineVolley,
+    RepairAgain
+}
