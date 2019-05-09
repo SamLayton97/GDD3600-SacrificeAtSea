2a3a441d9bf17dcadc588d23a2118465c90285c9
Sam Layton <slayton@uccs.edu>
2019-05-09 16:49:06 -0600

    Added an adaptability manager script to game manager object

--- GDD3600-SacrificeAtSea/Assets/Scripts/Manager Scripts/AdaptationManager.cs
+++ GDD3600-SacrificeAtSea/Assets/Scripts/Manager Scripts/AdaptationManager.cs
@@ -11,12 +11,25 @@ public class AdaptationManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // read performance data and change in-game variables accordingly
+        ReadAndAdapt();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Reads performance data from previous scene
+    /// and adapts accordingly.
+    /// </summary>
+    void ReadAndAdapt()
     {
-        
+        Debug.Log("READ AND ADAPT");
+    }
+
+    /// <summary>
+    /// Saves player performance-tracking data from
+    /// current level.
+    /// </summary>
+    void SavePerformanceData()
+    {
+        Debug.Log("SAVE PERFORMANCE DATA");
     }
 }
